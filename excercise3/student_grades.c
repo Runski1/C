27c162d05b1b52bb5febbbf245cc828d66fb8515
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2024-08-26 21:04:46 +0300

    excercise3 done

--- excercise3/student_grades.c
+++ excercise3/student_grades.c
@@ -1,55 +1,86 @@
 #include <stdio.h>
 
-int student_count = 0, selected_student = -1, default_grade = -1,
-    given_grade = 1;
-
 void clear_input_buffer(void) {
   while (getchar() != '\n')
     ;
 }
 
-int main(void) {
-  do {
-    printf("How many students: ");
-    if (scanf("%d", &student_count) != 1) {
+/* got bored of writing input validations
+ * so might aswell write a function for it
+ *
+ * I CBA to write one that can take different parameters in,
+ * for example strings for printf's and such. Did try at first,
+ * but The way some of the prompts for user input include nested integers,
+ * I would've had to craft strings before passing them in variables. so CBA*/
+int get_validated_int(void) {
+  int input, result;
+  char term;
+
+  while (1) {
+    result = scanf("%d%c", &input, &term);
+    /* not my own idea to find EOF in input buffer, but
+     * IIUC user might end input with ctrl+D or similar, which
+     * would in turn put EOF in the buffer*/
+    while (term != '\n' && term != EOF) {
       clear_input_buffer();
     }
-  } while (student_count == 0);
+    if (result == 2 &&
+        term == '\n') { // 2, not 1: scanf should match input AND term
+      // I think there's a bug here if user inputs EOF after valid int
+      return input;
+    }
+  }
+}
+
+int main(void) {
+
+  int student_count = 0, selected_student = -1, default_grade = -1,
+      given_grade = 1;
+
+  printf("How many students: ");
+  student_count = get_validated_int();
+  printf("Student's added: %d \n", student_count);
 
   int students[student_count]; // init students-array
   for (int i = 0; i < (sizeof(students) / sizeof(students[0])); i++) {
     students[i] = default_grade;
   }
 
-  do {
+  do { // loop to select students, 0 to print
     printf("Enter student number (1-%d) or 0 to stop: ", student_count);
-    if (scanf("%d", &selected_student) != 1) {
-      clear_input_buffer();
-    }
-  } while (selected_student == -1);
+    selected_student = get_validated_int();
 
-  if (selected_student == 0) { // final print
-    printf("%10s %10s", "Student", "Grade");
-    for (int i = 0; i < student_count; i++) {
-      if (students[i] != -1) {
-        printf("%10d %10d", i + 1, students[i]);
-      } else {
-        printf("%10d %10s", i + 1, "N/A");
+    if (selected_student == 0) { // final print
+      printf("%10s %10s\n", "Student", "Grade");
+      for (int i = 0; i < student_count; i++) {
+        if (students[i] != -1) {
+          printf("%10d %10d\n", i + 1, students[i]);
+        } else {
+          printf("%10d %10s\n", i + 1, "N/A");
+        }
       }
+      return 0; // Student selection loop is broken by the return statement, I
+                // wonder if it's ok...
+
+      /* Loop until valid grade is gotten */
+    } else if (0 < selected_student && selected_student <= student_count) {
+      while (1) {
+        printf("Enter grade (0-5) for student %d or -1 to cancel: ",
+               selected_student);
+        given_grade = get_validated_int();
+        if (0 <= given_grade &&
+            given_grade <= 5) { // -1 is NOT valid grade in this case, since it
+                                // would overwrite the grade
+          students[selected_student - 1] = given_grade;
+          break;
+        } else if (given_grade == -1) {
+          break;
+        } else {
+          printf("Invalid grade!\n");
+        }
+      }
+    } else {
+      printf("Invalid student number!\n");
     }
-    return 0;
-  } else {
-    do {
-      printf("Enter grade (0 - 5) for student %d or -1 to cancel: ",
-             selected_student);
-      if (scanf("%d", &given_grade) != 1) {
-        clear_input_buffer();
-      };
-      if (given_grade > 5) {
-        printf("Invalid grade!");
-        given_grade = -1;
-      }
-    } while (given_grade < -1 || given_grade > 5);
-    students[selected_student-1] = given_grade;
-  }
+  } while (selected_student != 0);
 }
