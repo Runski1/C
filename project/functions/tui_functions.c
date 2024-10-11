9e07ce0a7953b2994b929512a78eeb472bdffb21
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2024-10-11 09:43:28 +0300

    suvi

---
+++ project/functions/tui_functions.c
@@ -0,0 +1,39 @@
+#include "macros.h"
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *TUI(char **rooms) {
+    int choice = 0, i = 0;
+    do {
+        i = 0;
+        // CLEAR_TERMINAL //I'm debating on this
+        printf("Show room temperature records>\n");
+        while (i < MAX_ROOMS && rooms[i] != NULL) {
+            printf("(%d) %s\n", i + 1, rooms[i]);
+            i++;
+        }
+        choice = read_range(1, i, "");
+    } while (choice < 1 || choice > i); // i instead of i+1 since it's ++'d once
+    return rooms[choice - 1];
+}
+
+
+void print_temperature(char *choice, char *room_name, float *temperature) {
+    /* Do the nice print 
+     * LOW PRIORITY reduce chaining
+     * */
+    int i = 0;
+    if (strcmp(choice, room_name) == 0) {
+        printf("\n%4.1f\u00B0C ", *temperature);
+        if (*temperature >= 0 && *temperature <= 30) {
+            while (i < ((int)(*temperature * 2))) { // this trunks, good enough
+                printf("-");
+                i++;
+            }
+        } else {
+            printf("X");
+        }
+    }
+}
---
+++ project/functions/tui_functions.h
@@ -0,0 +1,10 @@
+#ifndef TUI_FUNCTIONS
+#define TUI_FUNCTIONS
+
+#include <stdio.h>
+
+void print_temperature(char *choice, char *room_name, float *temperature);
+char *TUI(char **rooms);
+
+#endif
+
